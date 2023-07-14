1ec559d5d6764c56dd42bb7c7cc7107bdea6ffff
ayoub <ayoub.alabed@gmail.com>
2023-07-14 20:20:16 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useContext, useState } from "react"
+import { Footer } from "./components/footer/Footer"
+import { Header } from "./components/header/Header"
+import { Home } from "./pages/home/Home"
+import { Login } from "./pages/login/Login"
+import { Regsiter } from "./pages/login/Regsiter"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { DetailsPages } from "./pages/details/DetailsPages"
+import { Account } from "./pages/account/Account"
+import { Create } from "./components/create/Create"
+import { Context } from "./context/Context"
+//import { Card } from "./components/blog/Card"
+//import { blog } from "./assets/data/data"
+
+
+const App = () => {
+  //const [allBlog, setAllBlog] = useState(blog)
+  //after login
+  const { user } = useContext(Context)
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Regsiter} />
+          <Route exact path='/post/:id' component={DetailsPages} />
+          <Route exact path='/account' component={Account} />
+          <Route exact path='/create' component={Create} />
+          {
+            //<Route path='/blog' component={<Card allBlog={allBlog} setAllBlog={setAllBlog} />}/>
+            }
+        </Switch>
+        <Footer />
+      </Router>
+    </>
+  )
+}
+export default App
