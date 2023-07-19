e1593bef1a9e3854afecbc1169af2cd595d9294c
ayoubdiver <124197124+ayoubdiver@users.noreply.github.com>
2023-07-20 00:12:50 +0100

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -14,7 +14,7 @@ export const Home = () => {
 
   useEffect(() => {
     const fetchPost = async () => {
-      const res = await axios.get("/posts" + search)
+      const res = await axios.get("posts" + search)
       setPosts(res.data)
     }
     fetchPost()
