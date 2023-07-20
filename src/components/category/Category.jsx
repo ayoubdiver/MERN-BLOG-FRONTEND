53283e7942d994f4dcc518ccecffcdf244515b6d
ayoubdiver <124197124+ayoubdiver@users.noreply.github.com>
2023-07-20 01:14:11 +0100

    Update Category.jsx

--- src/components/category/Category.jsx
+++ src/components/category/Category.jsx
@@ -55,7 +55,7 @@ export const Category = () => {
 
   useEffect(() => {
     const getCat = async () => {
-      const res = await axios.get("/category" + search)
+      const res = await axios.get("https://gosto-api.onrender.com/category" + search)
       setCat(res.data)
     }
     getCat()
