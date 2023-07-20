3b90579300ad6ef3453e63ba8cdcae492d8aceab
ayoubdiver <124197124+ayoubdiver@users.noreply.github.com>
2023-07-20 01:22:43 +0100

    Update data.js

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -9,21 +9,13 @@ export const nav = [
     text: "about",
     url: "/about",
   },
+  
   {
     id: 3,
-    text: "pages",
-    url: "/pages",
-  },
-  {
-    id: 4,
     text: "blog",
     url: "/blog",
   },
-  {
-    id: 5,
-    text: "contact",
-    url: "/contact",
-  },
+
 ]
 export const category = [
   {
